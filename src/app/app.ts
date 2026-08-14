5135954a1479e1d0790d500ee90d65117888d756
Elayabharathi <elayabharathi.mail@gmail.com>
2026-08-14 19:24:48 +0530

    Initial job recommendation frontend

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,80 @@
 import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { JobService, Job } from './services/job';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
+  imports: [
+    CommonModule,
+    FormsModule
+  ],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
-  protected readonly title = signal('job-recommendation-frontend');
+
+  userId = 1;
+
+  jobs = signal<Job[]>([]);
+
+  loading = signal(false);
+
+  error = signal('');
+
+  constructor(private jobService: JobService) {}
+
+  findJobs(): void {
+
+    console.log('Find Jobs clicked');
+
+    const id = Number(this.userId);
+
+    console.log('User ID:', id);
+
+    if (!id || id <= 0) {
+      this.error.set('Please enter a valid User ID.');
+      return;
+    }
+
+    // Start loading
+    this.loading.set(true);
+
+    // Clear old error
+    this.error.set('');
+
+    this.jobService.getRecommendedJobs(id).subscribe({
+
+      next: (response: Job[]) => {
+
+        console.log('Backend Response:', response);
+
+        // Store jobs
+        this.jobs.set(response);
+
+        // IMPORTANT: stop loading
+        this.loading.set(false);
+
+        console.log('Jobs:', this.jobs());
+        console.log('Loading:', this.loading());
+
+      },
+
+      error: (err) => {
+
+        console.error('API Error:', err);
+
+        this.jobs.set([]);
+
+        this.loading.set(false);
+
+        this.error.set(
+          'Unable to load recommended jobs.'
+        );
+
+      }
+
+    });
+  }
 }
---
+++ src/app/services/job.ts
@@ -0,0 +1,36 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+export interface Job {
+
+  job: string;
+
+  company: string;
+
+  location: string;
+
+  salary: number;
+
+}
+
+@Injectable({
+  providedIn: 'root'
+})
+export class JobService {
+
+  private apiUrl =
+    'https://job-recommendation-backend.onrender.com/api/jobs';
+
+  constructor(private http: HttpClient) {}
+
+  getRecommendedJobs(
+    userId: number
+  ): Observable<Job[]> {
+
+    return this.http.get<Job[]>(
+      `${this.apiUrl}/recommendations/${userId}`
+    );
+
+  }
+}
